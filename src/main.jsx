00b6531a81bdbac0acc0baa339344821adf0846b
FacundoDayne <operavgxgamer@gmail.com>
2024-08-15 18:57:47 +0800

    add: react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+    {
+        path: "/presence",
+        element: <HomePage />,
+    },
+]);
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
+);
