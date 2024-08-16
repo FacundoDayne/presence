a1e069f00648b7bfb3d9d9ef94f51d5c177d3475
FacundoDayne <102406203+FacundoDayne@users.noreply.github.com>
2024-08-16 15:12:45 +0800

    add: css stuff in preparation

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
 import {useState} from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 import {Link, Outlet} from "react-router-dom";
+import './App.css';
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
         <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-
-            <Link to="/presence/">Home</Link>
-            {" | "}
-            <Link to="/presence/Profile">Profile</Link>
-            {" | "}
-            <Link to="/presence/Another">Another</Link>
+            <nav>
+                <Link to="/presence/">Home</Link>
+                {" | "}
+                <Link to="/presence/Profile">Profile</Link>
+                {" | "}
+                <Link to="/presence/Another">Another</Link>
+            </nav>
 
             <Outlet />
 
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
         </>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import 'bootstrap/dist/css/bootstrap.css'; 
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import ProfilePage from "./pages/ProfilePage.jsx";
